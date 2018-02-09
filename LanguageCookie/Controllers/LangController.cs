0ec5a039354a225668c1b4b38a164bc17900b65a
Pearson <FPearson@justice.gc.ca>
2018-02-09 15:50:28 -0500

    t

---
+++ LanguageCookie/Controllers/LangController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageCookie.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace LanguageCookie.Controllers
+{
+    public class LangController : Controller
+    {
+        public IActionResult Index(string lang = "")
+        {
+            if (lang != "") {
+                Response.Cookies.Append("_gc_lang", lang, new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    Domain = ".fjgc-gccf.gc.ca"
+                });
+            } else
+            {
+                lang = Request.Cookies["_gc_lang"] == null ? "eng" : Request.Cookies["_gc_lang"];
+                
+                //Response.Cookies.Append("_gc_lang", lang, new CookieOptions
+                //{
+                //    HttpOnly = true,
+                //    Secure = true,
+                //    Domain = ".fjgc-gccf.gc.ca"
+                //});
+            }
+            
+            ViewBag.Message = lang;
+            return View();
+        }
+
+     
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- LanguageCookie/Startup.cs
+++ LanguageCookie/Startup.cs
@@ -34,7 +34,7 @@ namespace LanguageCookie
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                //app.UseExceptionHandler("/Lang/Error");
             }
 
             app.UseStaticFiles();
@@ -43,7 +43,8 @@ namespace LanguageCookie
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Lang}/{action=Index}/{lang?}");
+             
             });
         }
     }
